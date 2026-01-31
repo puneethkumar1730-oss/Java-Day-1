e6aa289ccff2852b73cb9fe02ea58ec614942888
R PUNEETH KUMAR <puneethkumar1730@gmail.com>
2026-01-31 14:27:34 +0530

    Add files via upload

---
+++ BasicVar.java
@@ -0,0 +1,16 @@
+import javax.swing.text.StyledEditorKit;
+
+public class BasicVar {
+public static void main(String [] args){
+    int a=100;
+    int b=200;
+    int c=300;
+    System.out.print(a);
+    System.out.print(b);
+    System.out.println(c);
+    System.err.print(a+b);
+    System.err.println(b-c);    
+    System.out.println("hello world");
+    System.out.println();
+}
+}
