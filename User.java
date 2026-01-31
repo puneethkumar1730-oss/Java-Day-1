e828578f488d661d2f6176d1b69571377af67463
R PUNEETH KUMAR <puneethkumar1730@gmail.com>
2026-01-31 15:36:42 +0530

    Add files via upload

---
+++ User.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class User {
+    public static void main(String [] args)
+    {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("enter your age:");
+    int age=sc.nextInt();
+     System.out.println("enter your age:");
+     int puneeth=sc.nextInt();
+     System.out.println("sum of age and puneeth is a");
+    int a=age+puneeth;
+        System.out.println(a);
+    }
+    
+}
