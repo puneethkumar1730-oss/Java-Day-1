88b773e028d312efd8f42ff91898768a59614b38
R PUNEETH KUMAR <puneethkumar1730@gmail.com>
2026-02-07 10:52:40 +0530

    Add files via upload

---
+++ Primitive.java
@@ -0,0 +1,16 @@
+class Primitive {
+    public static void main(String []args)
+    {
+        char studentname='A';
+        byte age=18;
+        char grade='B';
+        short marks=820;
+        char status='P';
+        System.out.println(studentname);
+        System.out.println(age);
+        System.out.println(grade);
+        System.out.println(marks);
+        System.out.println(status);
+
+}
+}
