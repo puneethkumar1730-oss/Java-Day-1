e4a3d38c43fc1b65c7f693dbe127f31d8c667257
R PUNEETH KUMAR <puneethkumar1730@gmail.com>
2026-02-07 14:18:18 +0530

    Add files via upload

---
+++ Logical.java
@@ -0,0 +1,15 @@
+public class Logical{
+    public static void main(String[] args) {
+        int marks=37;
+        if(marks>90 && marks<100)
+            System.out.println("yes");
+    else if(marks>35 || marks<89)
+        {
+            System.out.println("pass");
+    }
+    else
+        {
+        System.out.println("no");
+    }
+}
+}
