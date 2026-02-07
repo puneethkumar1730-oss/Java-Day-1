a8bbda46357c6eccd7b402bde57314b41c0a436d
R PUNEETH KUMAR <puneethkumar1730@gmail.com>
2026-02-07 14:45:21 +0530

    Add files via upload

---
+++ Operator.java
@@ -0,0 +1,25 @@
+public class Operator {
+    public static void main(String[] args) {
+        int a=10;
+        int b=6;
+        int c=3;
+         c+=2;
+         if(a%2==0){
+            System.out.println("even");
+        
+         }
+         else
+         {
+            System.out.println("ood");
+         }
+        System.out.println(a+b);
+        System.out.println(a==b);
+        System.out.println(++a);
+        System.out.println(~a);
+        System.out.println(a>>1);
+        System.out.println(c);
+        System.out.println(a>b && b>c);
+
+    }
+    
+}
