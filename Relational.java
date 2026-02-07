ed6d8750bdfce79e7a547e7d553e3e078af00d3a
R PUNEETH KUMAR <puneethkumar1730@gmail.com>
2026-02-07 13:54:54 +0530

    Add files via upload

---
+++ Relational.java
@@ -0,0 +1,13 @@
+public class Relational {
+    public static void main(String[] args) {
+        float a=5.1f;
+        float b=4.3f;
+        System.out.println(a==b);
+        System.out.println(a!=b);
+        System.out.println(a>b);
+        System.out.println(a>=b);
+        System.out.println(a<b);
+        System.out.println(a<=b);
+    }
+    
+}
