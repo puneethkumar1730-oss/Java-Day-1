401a921d3dd9c7b91b708adad362d7e466b628b6
R PUNEETH KUMAR <puneethkumar1730@gmail.com>
2026-02-14 10:41:47 +0530

    Add files via upload

---
+++ Positive.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Positive{
+    public static void main(String [] args){
+    Scanner sc=new Scanner(System.in);
+    System.out.println("enter the num :");
+    int num=sc.nextInt();
+        if(num>0)
+        {
+           System.out.println("positive"); 
+        }
+        else if(num<0)
+        {
+            System.out.println("negative");
+        }
+        else{
+            System.out.println("zero");
+        }
+    }
+}
