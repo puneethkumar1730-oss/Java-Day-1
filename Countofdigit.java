d5972948134ed36ae641985343275a220666dc1c
R PUNEETH KUMAR <puneethkumar1730@gmail.com>
2026-02-14 15:01:23 +0530

    Add files via upload

---
+++ Countofdigit.java
@@ -0,0 +1,12 @@
+class Countofdigit {
+    public static void main(String []args){
+        int num = 12345;
+        int count=0;
+        while( num!=0)
+        {
+            count++;
+            num=num/10;
+        }
+        System.out.println(count); 
+    }
+}
