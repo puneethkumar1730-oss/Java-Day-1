2ee107263eea2b024108cf1b14618af36f6bbf82
R PUNEETH KUMAR <puneethkumar1730@gmail.com>
2026-02-14 10:36:17 +0530

    Add files via upload

---
+++ Largest.java
@@ -0,0 +1,14 @@
+class Largest{
+    public static void main(String [] args)
+{
+    int a=21;
+    int b=32;
+    if(a>b)
+    {
+        System.out.println("largest num is a "+a);
+    }
+    else{
+        System.out.println("largest num is b"+b);
+    }
+}
+}
