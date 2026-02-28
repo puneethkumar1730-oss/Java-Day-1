07ed94940072c92091f44f38dd0e9663c961eb99
R PUNEETH KUMAR <puneethkumar1730@gmail.com>
2026-02-28 15:34:17 +0530

    Add files via upload

---
+++ User1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class User1{
+      public static void main(String[] args) {
+      Scanner sc=new Scanner(System.in);
+      System.out.println("enter your salary:");
+       float salary=sc.nextFloat();
+      System.out.println("enter your age:");
+      int age=sc.nextInt();
+      System.out.println("enter your name:");
+      String s=sc.next();
+      System.out.println(salary);
+      System.out.println(age);
+      System.out.println(s);
+      }
+    }
+
+      
