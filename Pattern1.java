8812c205f82c4da889ffd755a8c9909cd702e1c8
R PUNEETH KUMAR <puneethkumar1730@gmail.com>
2026-02-28 11:05:33 +0530

    Add files via upload

---
+++ Pattern1.java
@@ -0,0 +1,19 @@
+public class Pattern1{
+
+    public static void main(String [] args)
+    {
+        int i, j;
+        int n=5;
+        for (i = 0; i < n; i++) 
+            
+            {
+        for (j = 0; j < n; j++)
+            {
+             System.out.print(" * ");
+            }
+        System.out.println();
+        }
+    }
+}
+
+    
