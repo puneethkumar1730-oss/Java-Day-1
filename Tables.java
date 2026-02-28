d7fb83280e07d555ce4262a608a9448b3bd8e7c9
R PUNEETH KUMAR <puneethkumar1730@gmail.com>
2026-02-28 15:57:46 +0530

    Add files via upload

---
+++ Tables.java
@@ -0,0 +1,9 @@
+public class Tables {
+    public static void main(String[] args) {
+        int n=5;
+        int i=1;
+        for(i=1;i<=10;i++){
+            System.out.format("%d X %d=%d\n",n,i,n*i);
+        }
+ }
+}
