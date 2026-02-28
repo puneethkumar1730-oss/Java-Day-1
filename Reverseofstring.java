7f570a532e829779e0effbf1116404a3c6da0b91
R PUNEETH KUMAR <puneethkumar1730@gmail.com>
2026-02-28 15:47:20 +0530

    Add files via upload

---
+++ Reverseofstring.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Reverseofstring {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the string :");
+        String str=sc.nextLine();
+        String reversed="";
+        for(int i=str.length()-1;i>=0;i--){
+            reversed=reversed+str.charAt(i);
+        }
+        System.out.println("Reversed string:"+reversed);
+    }
+}
