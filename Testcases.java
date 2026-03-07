5d608f4cbce76ffec6e03fdac3ffa8655f7eca1b
R PUNEETH KUMAR <puneethkumar1730@gmail.com>
2026-03-07 13:26:58 +0530

    Add files via upload

---
+++ Testcases.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Testcases {
+    public static void main(String [] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the number");
+        int n=sc.nextInt();
+        for(int i=0;i<n;i++)
+        {
+            int a=sc.nextInt();
+
+            int b=sc.nextInt();
+            System.out.println("sum="+(a+b));
+        }    
+}
+
+}
